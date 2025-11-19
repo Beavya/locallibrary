0ed29b1e1fdb7975904c18f2dd480281b372cf5b
Beavya <AnnBezymya@gmail.com>
2025-11-19 14:56:23 +0700

    fix .gitignore

--- catalog/views.py
+++ catalog/views.py
@@ -16,7 +16,7 @@ def index(request):
     num_books=Book.objects.all().count()
     num_instances=BookInstance.objects.all().count()
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()
+    num_authors= Author.objects.count()
     num_genres = Genre.objects.count()
     num_language = Language.objects.count()
 
